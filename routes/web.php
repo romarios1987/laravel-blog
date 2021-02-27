b85c81f559447aea5ae57b1f819185e5d9483085
romarios1987 <romios047@gmail.com>
2021-02-27 23:21:13 +0200

    created first MainController for admin

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function (){
+    Route::get('/', 'MainController@index')->name('admin.index');
+});
